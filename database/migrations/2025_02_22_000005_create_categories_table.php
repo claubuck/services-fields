a0f99e025966e914bc1373f47fcbdcfb9ca9a542
claubuck <camposclaudio@live.com>
2026-04-07 09:55:38 -0300

    Add 'codigo' column to categories table in migration

--- database/migrations/2025_02_22_000005_create_categories_table.php
+++ database/migrations/2025_02_22_000005_create_categories_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('codigo')->nullable();
             $table->string('nombre');
             $table->timestamps();
         });
