cf6d6e2b4d5e21ed54f7458abe75ce769ed5b80f
claubuck <camposclaudio@live.com>
2026-04-07 09:55:53 -0300

    Add CategorySeeder to seed initial category data

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run(): void
+    {
+        Category::firstOrCreate(
+            ['codigo' => '009730'],
+            ['nombre' => 'COSECHADOR'],
+        );
+    }
+}
